2fa024e210de6725ae316e151a4b747eab4bd305
Koushik Sarkar <koushiks@nycstudents.net>
2022-09-30 11:57:37 -0400

    new changes

---
+++ src/CircleRunner.java
@@ -0,0 +1,18 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle obj = new Circle(5.0);
+
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        obj.printArea();
+
+
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //Prediction: 25pi
+        // D. run the code and confirm your prediction! Were you correct?
+        //
+    }
+}
